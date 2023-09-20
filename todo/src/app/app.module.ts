db7b3cd7b57a86d5ca1e5c1e0f442088ccd65efb
jojo <princessnoachi@gmail.com>
2023-09-20 17:35:54 +0800

    add

--- todo/src/app/app.module.ts
+++ todo/src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskModule } from './task/task.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 
 @NgModule({
@@ -14,6 +15,12 @@ import { TaskModule } from './task/task.module';
     BrowserModule,
     AppRoutingModule,
     TaskModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
  
   ],
   providers: [],
